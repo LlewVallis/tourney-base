e4b0461bce883d486b2b271f668ac4794c7d09ab
LlewVallis <llewvallis@gmail.com>
2022-02-02 20:45:17 +1100

    Add Google Search Console verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -50,6 +50,11 @@ export default class CustomDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
             rel="stylesheet"
           />
+
+          <meta
+            name="google-site-verification"
+            content="cU9BTL57mYDFdvmuonyaPQDpB9tyJsSmkllhfnvQi2A"
+          />
         </Head>
         <body>
           <Main />
